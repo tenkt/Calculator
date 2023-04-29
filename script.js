43f42bf93ddc6a339f7ca1f74fd0b7d972f5d22a
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-28 22:13:22 -0700

    Create operate() for basic math calculations with 'if' statements.

--- script.js
+++ script.js
@@ -23,17 +23,25 @@ function subtractMath(a, b) {
     return a - b;
 };
 
-function multiplyMath (array) {
-return array.length
-    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
-    : 0;
+function multiplyMath (a, b) {
+return a * b;
 };
 
 function divideMath(a, b) {
     return a / b;
 }
 
-function operate (num1, num2) {
-    let num1, num2, operator;
-    return 
+function operate (num1, num2, operator) {
+    if (operator === addMath) {
+        return addMath(num1, num2);
+    }
+    if (operator === subtractMath) {
+        return subtractMath(num1, num2);
+    }
+    if (operator === multiplyMath) {
+        return multiplyMath(num1, num2);
+    }
+    if (operator === divideMath) {
+        return divideMath(num1, num2);
+    }
 }
