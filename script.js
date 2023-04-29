e2b6b098f8ebf0f291823a7e1cfa90dac556551a
tenk <119652603+tenkt@users.noreply.github.com>
2023-04-28 21:38:47 -0700

    Comment out factgorialMath() and sumMath() for future implementation as it's not needed for basic math.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// function factorialMath (n) {
+// if (n === 0) return 1;
+// let product = 1;
+// for (let i = n; i > 0; i--) {
+//    product *= i;
+// }
+// return product;
+// };
+
+//function sumMath (array) {
+//    return array.reduce((total, current) => total + current, 0);
+//  };
+
 function addMath(a, b) {
     return a + b;
 };
@@ -6,10 +19,6 @@ function subtractMath(a, b) {
     return a - b;
 };
 
-function sumMath (array) {
-    return array.reduce((total, current) => total + current, 0);
-  };
-  
 function multiplyMath (array) {
 return array.length
     ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
@@ -20,11 +29,3 @@ function powerMath (a, b) {
 return Math.pow(a, b);
 };
 
-function factorialMath (n) {
-if (n === 0) return 1;
-let product = 1;
-for (let i = n; i > 0; i--) {
-    product *= i;
-}
-return product;
-};
