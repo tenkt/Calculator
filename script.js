6f2d199d214de41445af987312bfb1d41f367eaf
Teng Thao <119652603+tenkt@users.noreply.github.com>
2023-05-03 00:38:37 -0700

    Revise global variables. Add querySelectors for different types of buttons. Revise click buttons to display whole equation. Split equation into an array and assign values to operate() variables. Add operator(). Edit clear().

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-let num1 = '';
-let num2 = '';
-let currentValue = '';
-const container = document.querySelector('.display');
-const clearAll = document.querySelector('.clear');
-const signs = document.querySelectorAll('.symbol');
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+//let numArray = []
+let currentDisplay = document.querySelector('.current');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const operators = document.querySelectorAll('.opera');
 const buttons = document.querySelectorAll('.button');
-    
+
+
+
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        container.textContent += button.dataset.value; 
+    button.addEventListener('click', (e) => {
+        equation = currentDisplay.textContent += button.dataset.value;
+        numArray = equation.split(" ")
+        firstNum = numArray[0]
+        operator = numArray [1]
+        secondNum = numArray[2]
+        equal.addEventListener('click', () => {
+            operate(firstNum, secondNum, operator)
+        })
     });
 });
-signs.forEach((symbol) => {
-    symbol.addEventListener('click', (e) => {
-        operator = symbol.dataset.value;
-        currentValue = num1 + operator
-        console.log(currentValue)
+
+
+
+function opHold(opera) {
+    operator = opera
+    currentDisplay.textContent += " " + operator + " "
+   
+}
+console.log(currentDisplay.textContent)
+operators.forEach((opera) => {
+    opera.addEventListener('click', (e) => {
+        opHold(e.target.textContent);
+        console.log(currentDisplay.textContent)
     });
 });
+
+
 
 function operate(num1, num2, operator) {
     if (operator = "+") {
@@ -47,9 +68,8 @@ function divideMath(num1, num2) {
     return num1 / num2;
 }
 
-clear = () => {
-    return container.textContent = '';
-};
-clearAll.addEventListener('click', () => {
-    clear();
-})
+clear.addEventListener('click', () => {
+    currentDisplay.textContent = '';
+    firstNum = '';
+    operator = '';
+});
